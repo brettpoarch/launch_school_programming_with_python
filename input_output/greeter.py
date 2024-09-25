d69b443fc9513a9f6e5a4c35c177c543d56b0480
Brett Poarch <brett.poarch@gmail.com>
2024-09-25 20:56:56 +0000

    modified to ask for first and last name

--- input_output/greeter.py
+++ input_output/greeter.py
@@ -1,4 +1,8 @@
-name = input('What is your name?\n')
+# excercise 1 Write a program named greeter.py. The program should ask for your name, then output Hello, NAME! where NAME is the name you entered:
+# excercise 2 Modify the greeter.py program to ask for the user's first and last names separately, then greet the user with their full name.
 
-print(f'Hello, {name}!')
+first_name = input('What is your first name?\n')
+last_name = input('What is your last name?\n')
+
+print(f'Hello, {first_name} {last_name}!')
 			 
