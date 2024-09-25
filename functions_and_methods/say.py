a2e6829198aa2e7857b3c4d7c325787b69c20789
Brett Poarch <brett.poarch@gmail.com>
2024-09-25 22:04:27 +0000

    created say.py and say2.py

---
+++ functions_and_methods/say.py
@@ -0,0 +1,17 @@
+def say():
+	print('Output from say')
+
+print('First')
+say()
+print('Last')
+
+'''
+greeting = 'Salutations'
+
+def well_howdy(who):
+    greeting = 'Howdy'
+    print(f'{greeting}, {who}')
+
+well_howdy('Angie')
+print(greeting)
+'''
