271a8e8a8478a7f94e601bdce8cd1c63ed3d78b2
Brett Poarch <brett.poarch@gmail.com>
2024-09-27 21:44:12 +0000

    started excercises for additional topics

---
+++ additional_topics/excercises.py
@@ -0,0 +1,11 @@
+def page_break():
+	print('-' * 45)
+
+# excercise 1 In your own words, explain the difference between these two expressions.
+
+print('expression 1 is checking to see if the objects have the same values. expression 2 checks to compare if they are he same object')
+
+page_break()
+
+# excercise 2
+
