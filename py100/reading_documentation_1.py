2ccde5d76002be061229952d652e2116ad44accd
Brett Poarch <brett.poarch@gmail.com>
2024-09-28 14:36:57 +0000

    started excercises for PY100 reading documentation 1

---
+++ py100/reading_documentation_1.py
@@ -0,0 +1,13 @@
+print('Question 1: What is the official place to find Python documentation? The documentaion can be found at https://docs.python.org/3/ ')
+
+print('Question 2: Determine whether Python has a method to lowercase a string, for example converting \'Aloha, World!\' into \'aloha, world!.\n\
+Yes, Python has the str.lower() method')
+
+print('Question 3: Use the Python documentation for the str class to determine which method can be used to right justify a str object.\n\
+ - the str.rjust() method would be the method we use here')
+
+print('Question 4: Is there a method to reverse a string, for example turning \'hello\' into \'olleh\'? - No, there is not a method to reverse a str')
+
+print('Question 5: How can we access the second element (\'and\') in the list [\'fish\', \'and\', \'chips\']? - we can access it with list[1]')
+
+print('Question 6: How would you determine the index of the fruit "cherry" in this list? fruits.index(\'cherry\')')
